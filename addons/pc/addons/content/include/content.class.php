bfd77439dac03d63f35b16f6afe58a79fc28ba0d
chguoxi <995306681@qq.com>
2016-10-29 14:09:36 +0800

    content manage

---
+++ addons/pc/addons/content/include/content.class.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | phpWeChat content 操作类 Last modified 2016-10-29 12:34:34
+// +----------------------------------------------------------------------
+// | Copyright (c) 2009-2016 phpWeChat http://www.phpwechat.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 骑马的少年 <phpwechat@126.com> <http://www.phpwechat.com>
+// +----------------------------------------------------------------------
+namespace pc\Content;
+use phpWeChat\Area;
+use phpWeChat\CaChe;
+use phpWeChat\Config;
+use phpWeChat\DataInput;
+use phpWeChat\DataList;
+use phpWeChat\Member;
+use phpWeChat\Module;
+use phpWeChat\MySql;
+use phpWeChat\Order;
+use phpWeChat\Upload;
+
+class Content
+{
+	public static $mPageString=''; // 这个静态成员是系统自带，请勿删除
+	
+
+}
+?>
